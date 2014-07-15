fd7b6d4bcaa870442c90f622c5122792e72e5dbc
Frank Paz <fpaz@paypal.com>
2014-07-15 18:05:50 +0800

    Update images and workflow

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,22 @@
 function navigateTo(step) {
+	if(step == "install-screen") {
+		if(!$("#install-NNL").hasClass("checked")) {
+			return false;
+		}
+	}
+
+	if(step == "loading-screen") {
+		if($("input[name=email]").val() == "" || $("input[name=password]").val() == ""){
+			return false;	
+		}
+	}
+
+	if(step == "confirm") {
+		if(!$("#install-PP").hasClass("checked")){
+			step = "install-paypal-app";
+		}
+	}
+
 	history.pushState(step, step, '#' + step);
 	
 	window.location.href= '#' + step;
@@ -11,13 +29,13 @@ function navigateTo(step) {
 function loaderInstall() {
 	var stepId = window.location.href.split('#')[1];
 
-	if(stepId && (stepId == 'install-screen' || stepId == 'loading-screen')){
+	if(stepId && (stepId == 'install-screen' || stepId == 'loading-screen' || stepId == 'install-paypal-app-screen')){
 		setTimeout(function(){
 			var nextId = $('#' + stepId).next().attr("id");
 			navigateTo(nextId);
 			vibrate(500);
-		}, 1500);
-	}	
+		}, 5000);
+	}
 }
 
 function vibrate(ms) {
@@ -34,7 +52,7 @@ function onDeviceReady(){
 		return false;
 	});
 
-	$("#register-swipe").on('swipedown', function(e,data){
+	$("#register-swipe").bind('touchend', function(e){
 		var indicator = $("ul#indicator");
 		var boxSize = indicator.find("li").size();
 
@@ -45,14 +63,26 @@ function onDeviceReady(){
 			indicator.append("<li></li>");
 			vibrate(80);
 		}
-	}).on('touchmove touchstart touchend', function(e){e.preventDefault();});
+	});
 
-	$("#link-swipe").on('swipedown', function(e,data){
+	$("#link-swipe").bind('touchend', function(e){
 		navigateTo('confirm');
 		vibrate(500);
-	}).on('touchmove touchstart touchend', function(e){e.preventDefault();});
+	});
+
+	$(".checkbox").click(function(){
+		$(this).toggleClass("checked");
+	});
+
+	$("#use-paypal").click(function(){
+		$(this).toggleClass("toggle-on");
+	});
 
 	window.addEventListener("popstate", loaderInstall);
+	
+	document.addEventListener("resume", function(){
+		window.location.href="index.html";
+	}, false);
 
 	loaderInstall();
 }
